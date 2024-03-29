6f37ee56420d75ac0137de369c48f57f6805bac6
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:31:40 +0700

    Add project files.

---
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
+int n;
+
