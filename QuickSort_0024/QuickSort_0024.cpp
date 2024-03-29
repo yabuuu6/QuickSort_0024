0338a391d6d0ecb55dc63adb56f70043290441cf
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:34:45 +0700

    commit 5

--- QuickSort_0024/QuickSort_0024.cpp
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -28,3 +28,15 @@ void input() {
 		cout << "<" << (i + 1) << ">";
 		cin >> arr[i];
 	}
+}
+		//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
+
+}
+
