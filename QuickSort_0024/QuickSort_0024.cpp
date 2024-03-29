fc9c88a260ee26909c53ebdbedc53109f3815063
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:33:02 +0700

    commit 2

--- QuickSort_0024/QuickSort_0024.cpp
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -6,4 +6,15 @@ int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
- 
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
