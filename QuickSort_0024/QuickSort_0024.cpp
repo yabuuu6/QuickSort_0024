9486a459a7f0de3cd0bbcb0934e3c0550256c871
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:33:39 +0700

    commit 3

--- QuickSort_0024/QuickSort_0024.cpp
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -18,3 +18,8 @@ void input() {
 		else
 			cout << "\nMaksimum panjang array adalah 20" << endl;
 	}
+
+	cout << "\n------------------- " << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n------------------- " << endl;
+
