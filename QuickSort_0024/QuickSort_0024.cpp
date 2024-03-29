c4f626ef94040042b082b5c34d4140256cd08e98
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:37:17 +0700

    commit 10

--- QuickSort_0024/QuickSort_0024.cpp
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -69,3 +69,11 @@ void q_short(int low, int high)
 			j--; //step 8
 			cmp_count++;
 		}
+		cmp_count++;
+		if (i < j)   // step 9
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+		}
+	}
+	
