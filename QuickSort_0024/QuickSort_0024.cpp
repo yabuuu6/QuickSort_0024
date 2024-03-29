1a9ab75b5390bcf03070f212686e280023e96286
buyaramadhan <ytchochote@gmail.com>
2024-03-29 20:38:09 +0700

    commit 12

--- QuickSort_0024/QuickSort_0024.cpp
+++ QuickSort_0024/QuickSort_0024.cpp
@@ -81,4 +81,14 @@ void q_short(int low, int high)
 		swap(low, j);
 	}
 
-	
+	//recursive call to sort the Left sub array
+
+	q_short(low, j - 1); //step 12
+
+	//recursive call to sort the right sub array
+
+	q_short(j + 1, high); //step 13
+
+
+}
+
